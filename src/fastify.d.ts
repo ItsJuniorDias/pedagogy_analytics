3415c35f111838061a88fc58b10dcd53b3f50732
Alexandre Junior <reabilitado97@gmail.com>
2026-07-05 16:44:22 -0300

    Revert "chore: add meta capi"
    This reverts commit 95664f8dfc4bd20982dd8a0220591abc7901d5e8.

--- src/fastify.d.ts
+++ src/fastify.d.ts
@@ -1,9 +1,7 @@
 import type { Store } from "./db/types";
-import type { MetaCapi } from "./lib/metaCapi";
 
 declare module "fastify" {
   interface FastifyInstance {
     store: Store;
-    metaCapi: MetaCapi;
   }
 }
